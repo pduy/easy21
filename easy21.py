183201b3af82048845ebca8be78aef92734e5a52
Duy Pham <pham.duy89@gmail.com>
2019-02-19 12:28:58 +0100

    implement the game

--- easy21.py
+++ easy21.py
@@ -13,7 +13,7 @@ class Card:
         self.val = val if self.color == Color.black else -val
 
 
-class BasePlayer:
+class BasePlayer(object):
 
     def __init__(self, cards):
         self.cards = cards
@@ -32,22 +32,31 @@ class BasePlayer:
 
 class Player(BasePlayer):
 
-    def __init__(self):
-        super(Player, self).__init__()
+    def __init__(self, cards):
+        super(Player, self).__init__(cards)
 
 
 class Dealer(BasePlayer):
-
-    def __init__(self):
-        super(Dealer, self).__init__()
+    def __init__(self, cards):
+        super(Dealer, self).__init__(cards)
 
     def act(self):
-        if self.val() < 17:
+        while self.val() < 17:
             self.hit()
 
 
-# class State:
-#     def __init__(self, dealer, player):
+class State:
+    def __init__(self, dealer, player):
+        self.dealer = dealer
+        self.player = player
+
+    def take_action(self, player_action):
+        if player_action == 'hit':
+            self.player.hit()
+        else:
+            self.dealer.act()
+
+        return State(self.dealer, self.player)
 
 
 def draw(color=None):
@@ -59,5 +68,31 @@ def draw(color=None):
         if color is None else color)
 
 
-def step(state, action):
-    return ''
+def step(state, player_action):
+    return state.take_action(player_action)
+
+
+def init_game():
+    first_dealer = Dealer([draw(color=Color.black)])
+    first_player = Player([draw(color=Color.black)])
+    return State(first_dealer, first_player)
+
+
+def info(state):
+    print('dealer value = {0}'.format(state.dealer.val()))
+    print('player value = {0}'.format(state.player.val()))
+
+
+def main():
+    state = init_game()
+
+    info(state)
+
+    while True:
+        action = raw_input('enter your action:')
+        state = step(state, action)
+        info(state)
+
+
+if __name__ == "__main__":
+    main()
